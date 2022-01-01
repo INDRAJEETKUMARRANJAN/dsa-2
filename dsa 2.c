63b5b25924360a17a38433d10a34f21927d7fd6d
INDRAJEET KUMAR RANJAN 202086 <90103702+INDRAJEETKUMARRANJAN@users.noreply.github.com>
2022-01-01 17:30:54 +0530

    Add files via upload

---
+++ dsa 2.c
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+int binarySearch(int arr[], int l, int r, int x)
+{
+   if (r >= l)
+   {
+        int mid = l + (r - l)/2;
+
+
+        if (arr[mid] == x)  return mid;
+
+
+        if (arr[mid] > x) return binarySearch(arr, l, mid-1, x);
+
+
+        return binarySearch(arr, mid+1, r, x);
+   }
+
+
+}
+
+
+
+int bSearch(int arr[], int l, int r, int x)
+{
+  while (l <= r)
+  {
+    int m = l + (r-l)/2;
+
+
+    if (arr[m] == x)
+        return m;
+
+
+    if (arr[m] < x)
+        l = m + 1;
+
+
+    else
+         r = m - 1;
+  }
+
+
+  return -1;
+}
+
+int main(void)
+{
+   int arr[] = {2, 3, 4, 10, 40};
+   int n = sizeof(arr)/ sizeof(arr[0]);
+   int x = 10;
+   int result = binarySearch(arr, 0, n-1, x);
+   (result == -1)? printf("Element is not present in array")
+                 : printf("Element is present at index %d", result);
+
+   return 0;
+}
